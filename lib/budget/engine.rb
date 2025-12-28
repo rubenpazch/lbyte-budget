134cd1133f985c499825aeee4f268898c50fd636
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2025-12-28 14:33:25 -0500

    Add eager loading for models in dev/test; add lbyte-budget shim; fix autoloading for consuming apps

--- lib/budget/engine.rb
+++ lib/budget/engine.rb
@@ -7,10 +7,8 @@ module Budget
     isolate_namespace Budget
 
     # Ensure app directory is in autoload paths
-    config.autoload_paths += %W[
-      #{root}/app/models
-      #{root}/app/controllers
-    ]
+    config.autoload_paths << "#{root}/app/models"
+    config.autoload_paths << "#{root}/app/controllers"
 
     # Add views path for JBuilder templates
     config.paths['app/views'] ||= []
@@ -20,6 +18,13 @@ module Budget
       g.test_framework :rspec
       g.fixture_replacement :factory_bot
       g.factory_bot dir: 'spec/factories'
+    end
+
+    # Ensure models are loaded for associations in consuming apps
+    initializer 'budget.eager_load_models', before: :set_autoload_paths do
+      if Rails.env.development? || Rails.env.test?
+        config.eager_load_paths << "#{root}/app/models"
+      end
     end
   end
 end
